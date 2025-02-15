87cab120d41a8bf8aabeb4387b8f4505bff07c86
Balun <balunya232000@gmail.com>
2025-02-15 14:41:10 +0300

    Add homework number 3

---
+++ homework3/Calculator2num/Calculator2num/Calculator2num.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+
+class Calculator {
+	double num1, num2;
+	
+public:
+	double add() { return num1 + num2; }
+
+	double multiply() { return num1 * num2; }
+
+	double subtract_1_2() { return num1 - num2; }
+
+	double subtract_2_1() { return num2 - num1; }
+
+	double divide_1_2() { return num1 / num2; }
+
+	double divide_2_1() { return num2 / num1; }
+
+	bool set_num1(double num1) {
+		if (num1 != 0) {
+			this->num1 = num1;
+			return true;
+		}
+		return false;
+	}
+
+	bool set_num2(double num2) {
+		if (num2 != 0) {
+			this->num2 = num2;
+			return true;
+		}
+		return false;
+	}
+};
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	double n1{}, n2{};
+	Calculator c1{};
+	while (true) {
+
+		std::cout << "Ведите num1: ";
+		std::cin >> n1;
+
+		while (!c1.set_num1(n1)) {
+			std::cout << "Некорректно введенные данные! Попробуйте снова!\n";
+			std::cout << "Ведите num1: ";
+			std::cin >> n1;
+		}
+
+		std::cout << "Ведите num2: ";
+		std::cin >> n2;
+
+		while (!c1.set_num2(n2)) {
+			std::cout << "Некорректно введенные данные! Попробуйте снова!\n";
+			std::cout << "Ведите num2: ";
+			std::cin >> n2;
+		}
+
+		std::cout << "num1 + num2 = " << c1.add() << std::endl;
+		std::cout << "num1 * num2 = " << c1.multiply() << std::endl;
+		std::cout << "num1 - num2 = " << c1.subtract_1_2() << std::endl;
+		std::cout << "num2 - num1 = " << c1.subtract_2_1() << std::endl;
+		std::cout << "num1 / num2 = " << c1.divide_1_2() << std::endl;
+		std::cout << "num2 / num1 = " << c1.divide_2_1() << std::endl;
+
+	}
+	
+	return 0;
+}
---
+++ homework3/Counter/Counter/Counter.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+
+class Counter {
+	int value;
+public:
+	void incrementor() { value++; } 
+	void decrementor() { value--; }
+	int getvalue() const { return value; }
+	void set_default_val() { value = 1; }
+	void set_user_val(int userval) { value = userval; }
+};
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int startval{};
+	std::string choise{};
+	std::string command{};
+	Counter mycount1{};
+	
+	while (true) {
+		std::cout << "Желаете ли вы установить собственное значение? Введите y или n: ";
+		std::cin >> choise;
+		if (choise == "y" || choise == "Y") {
+			std::cout << "Введите начальное значение: ";
+			std::cin >> startval;
+			mycount1.set_user_val(startval);
+			break;
+		}
+		else if(choise == "n" || choise == "N")
+		{
+			mycount1.set_default_val();
+			break;
+		}
+		else {
+			std::cout << "Введено некорректное значение.Попробуйте снова!\n";
+		}
+	}
+
+	while (!(command == "x")) {
+		std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+		std::cin >> command;
+		if (command == "+") {
+			mycount1.incrementor();
+		}
+		else if (command == "-") {
+			mycount1.decrementor();
+		}
+		else if (command == "=") {
+			std::cout << "Ваше значение равно: " << mycount1.getvalue() << std::endl;
+		} 
+		else if (command == "x") {
+			std::cout << "До свидания!\n";
+		}
+		else {
+			std::cout << "Нераспознанная команда.\n";
+		}
+		
+	}
+	
+	return 0;
+}
