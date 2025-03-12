da294420ef995bc8856ab176a0fe178e5b96b1d2
Denis Balunin <balunya232000@gmail.com>
2025-03-12 14:12:02 +0300

    Add zadanie1 h7

---
+++ homework7/zadanie1/zadanie1/zadanie1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+#define MODE 1
+
+#ifndef MODE
+#error MODE is not defined!
+#endif
+
+int add(int x, int y) {
+	return x + y;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+#if MODE == 0
+		std::cout << "Работаю в режиме тренировки.\n";
+
+#elif MODE == 1
+		int a{}, b{};
+		std::cout << "Работаю в боевом режиме.\n";
+		std::cout << "Введите два числа!\n";
+		std::cin >> a >> b;
+		std::cout << "Результат сложения: " << add(a, b) << std::endl;
+#else
+		std::cout << "Неизвестный режим. Завершение работы.\n";
+#endif
+	return 0;
+}
