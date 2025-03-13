768d0c31b73bb56a61457bfbfef730aed103798e
Denis Balunin <balunya232000@gmail.com>
2025-03-13 15:45:44 +0300

    Add zadanie1 h8

---
+++ homework8/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+int function(std::string str, int forbidden_length) {
+    if (!(forbidden_length == str.length())) {
+        return str.length();
+    }
+    throw std::string{ "Вы ввели слово запретной длины! До свидания." };
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    std::string strValue{};
+    int length{};
+    std::cout << "Введите запретную длину\n";
+    std::cin >> length;
+
+    do {
+        std::cout << "Введите слово\n";
+        std::cin >> strValue;
+
+        try {
+            std::cout << "Длина слова " << strValue << " = "
+                << function(strValue, length) << std::endl;
+        }
+        catch (std::string& error_messege) {
+            std::cout << error_messege << std::endl;
+        }
+    } while (strValue.length() != length);
+
+    return 0;
+}
