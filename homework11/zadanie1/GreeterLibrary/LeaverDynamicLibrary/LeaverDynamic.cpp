ad0521102630d564ef8d3b676d6358256c977f44
Denis Balunin <balunya232000@gmail.com>
2025-03-21 12:33:29 +0300

    Deleted trash files

--- homework11/zadanie1/GreeterLibrary/LeaverDynamicLibrary/LeaverDynamic.cpp
+++
@@ -1,8 +0,0 @@
-#include "LeaverDynamic.h"
-namespace LeaverDynamicLibrary
-{
-	std::string Leaver::leave(std::string name) {
-		std::string leave = "Goodbye, " + name + "!";
-		return leave;
-	}
-}
