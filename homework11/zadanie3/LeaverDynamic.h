fe2270ce29fb5ec82948550e415b2d80256ec0c5
Denis Balunin <balunya232000@gmail.com>
2025-03-21 12:46:47 +0300

    Update zadanie3 h11

--- homework11/zadanie3/LeaverDynamic.h
+++ homework11/zadanie3/LeaverDynamic.h
@@ -1,14 +1,10 @@
 #pragma once
 #include <string>
-#ifdef LEAVERDYNAMICLIBRARY_EXPORTS
-#define LEAVERDYNAMICLIBRARY_API __declspec(dllexport)
-#else
-#define LEAVERDYNAMICLIBRARY_API __declspec(dllimport)
-#endif
+
 namespace LeaverDynamicLibrary
 {
 	class Leaver {
 	public:
-		LEAVERDYNAMICLIBRARY_API std::string leave(std::string name);
+		std::string leave(std::string name);
 	};
 }
